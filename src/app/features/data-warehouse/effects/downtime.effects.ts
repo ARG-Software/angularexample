dc8b88b8e0804ff3ebc5bd181062d0cbc233a86d
blekso <mihael.istvan@gmail.com>
2025-02-20 12:19:36 +0100

    fixed downtime.effects.ts

--- src/app/features/data-warehouse/effects/downtime.effects.ts
+++ src/app/features/data-warehouse/effects/downtime.effects.ts
@@ -1,13 +1,8 @@
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/mergeMap";
-
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { Actions, Effect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 
 import { catchError, switchMap, map, tap, finalize } from "rxjs/operators";
-
-import { of } from "rxjs/observable/of";
 
 import * as MimsModels from "@api/models/apimodels";
 
@@ -28,103 +23,114 @@ import * as DowntimeModelsUI from "../models/downtime.models";
 import { IDownTimeMachiningService } from "@api/services/interfaces/core/data-warehouse/idowntime.service";
 import { IMachineService } from "@api/services/interfaces/core/imachine.service";
 import { IProductService } from "@api/services/interfaces/core/iproduct.service";
-import { forkJoin } from "rxjs/observable/forkJoin";
-
-import { MimsSelectBoxModel } from "@mimsUI/input/select-box/models/select-box.model";
 import { DWMachiningDowntimeScreenRequestDto } from "@api/models/apimodels";
+import { forkJoin, of } from "rxjs";
 
 @Injectable()
 export class DowntimeEffects {
-  @Effect()
-  public getDowntimeData$ = this.actions$.pipe(
-    ofType<GetDowntimeData>(DowntimeActionTypes.GetDowntimeData),
-    tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
-    map((action) => {
-      const request: DWMachiningDowntimeScreenRequestDto = {
-        DWMachiningFilterDto: {
-          StartDate: new Date(action.payload.Filters.StartDate),
-          EndDate: new Date(action.payload.Filters.EndDate),
-          MachineId: action.payload.Filters.MachineId,
-          ProductId: action.payload.Filters.ProductId,
-        },
-        Paging: action.payload.Paging,
-      };
-
-      return request;
-    }),
-    switchMap((payload) =>
-      this.downtimeService.GetDownTimeData(payload).pipe(
-        map((response: MimsModels.IMachineDowntimeScreenDto) => {
-          const downtimeData: DowntimeModelsUI.DowntimeDataModelUI = {
-            Chart: converApiDataToChartData(response.ChartData),
-            Table: convertApiDataToTableData(response.TableData),
-          };
-
-          return new GetDowntimeDataSuccess(downtimeData);
-        }),
-        finalize(() =>
-          this.mainStore$.dispatch(new loadingActions.HideLoading())
-        ),
-        catchError((error) => {
-          return of(new DowntimeFailure(error));
-        })
-      )
-    )
-  );
-
-  @Effect({ dispatch: false })
-  public getDowntimeDataSuccess$ = this.actions$.pipe(
-    ofType<GetDowntimeDataSuccess>(DowntimeActionTypes.GetDowntimeDataSuccess)
-  );
-
-  @Effect()
-  public getDowntimeDataSelectBox$ = this.actions$.pipe(
-    ofType<GetDowntimeDataSelectBoxes>(
-      DowntimeActionTypes.GetDowntimeDataSelectBoxes
-    ),
-    tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
-    switchMap(() =>
-      forkJoin(
-        this.machineService.GetMachines(),
-        this.productService.GetProductsList()
-      ).pipe(
-        map((response: any[]) => {
-          return new GetDowntimeDataSelectBoxesSuccess(
-            convertApiDataToSelectBoxes(response)
-          );
-        }),
-        finalize(() =>
-          this.mainStore$.dispatch(new loadingActions.HideLoading())
-        ),
-        catchError((error) => {
-          return of(new DowntimeFailure(error));
-        })
-      )
-    )
-  );
-
-  @Effect({ dispatch: false })
-  public getDowntimeDataSelectBoxSuccess$ = this.actions$.pipe(
-    ofType<GetDowntimeDataSelectBoxesSuccess>(
-      DowntimeActionTypes.GetDowntimeDataSelectBoxesSuccess
-    )
-  );
-
-  @Effect({ dispatch: false })
-  public downtimeFailure$ = this.actions$.pipe(
-    ofType<DowntimeFailure>(DowntimeActionTypes.DowntimeFailure),
-    tap((error) => {
-      console.log("error");
-    })
-  );
-
-  public constructor(
+  constructor(
     private downtimeService: IDownTimeMachiningService,
     private machineService: IMachineService,
     private productService: IProductService,
     private actions$: Actions,
     private mainStore$: Store<fromMain.MainState>
   ) {}
+
+  public getDowntimeData$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<GetDowntimeData>(DowntimeActionTypes.GetDowntimeData),
+      tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
+      map((action) => {
+        const request: DWMachiningDowntimeScreenRequestDto = {
+          DWMachiningFilterDto: {
+            StartDate: new Date(action.payload.Filters.StartDate),
+            EndDate: new Date(action.payload.Filters.EndDate),
+            MachineId: action.payload.Filters.MachineId,
+            ProductId: action.payload.Filters.ProductId,
+          },
+          Paging: action.payload.Paging,
+        };
+
+        return request;
+      }),
+      switchMap((payload) =>
+        this.downtimeService.GetDownTimeData(payload).pipe(
+          map((response: MimsModels.IMachineDowntimeScreenDto) => {
+            const downtimeData: DowntimeModelsUI.DowntimeDataModelUI = {
+              Chart: converApiDataToChartData(response.ChartData),
+              Table: convertApiDataToTableData(response.TableData),
+            };
+
+            return new GetDowntimeDataSuccess(downtimeData);
+          }),
+          finalize(() =>
+            this.mainStore$.dispatch(new loadingActions.HideLoading())
+          ),
+          catchError((error) => {
+            return of(new DowntimeFailure(error));
+          })
+        )
+      )
+    )
+  );
+
+  public getDowntimeDataSelectBox$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<GetDowntimeDataSelectBoxes>(
+        DowntimeActionTypes.GetDowntimeDataSelectBoxes
+      ),
+      tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
+      switchMap(() =>
+        forkJoin({
+          machines: this.machineService.GetMachines(),
+          products: this.productService.GetProductsList(),
+        }).pipe(
+          map(({ machines, products }) => {
+            return new GetDowntimeDataSelectBoxesSuccess(
+              convertApiDataToSelectBoxes([machines, products])
+            );
+          }),
+          finalize(() =>
+            this.mainStore$.dispatch(new loadingActions.HideLoading())
+          ),
+          catchError((error) => {
+            return of(new DowntimeFailure(error));
+          })
+        )
+      )
+    )
+  );
+
+  public downtimeFailure$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<DowntimeFailure>(DowntimeActionTypes.DowntimeFailure),
+        tap((error) => {
+          console.log("Error:", error);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  public getDowntimeDataSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<GetDowntimeDataSuccess>(
+          DowntimeActionTypes.GetDowntimeDataSuccess
+        )
+      ),
+    { dispatch: false }
+  );
+
+  public getDowntimeDataSelectBoxSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<GetDowntimeDataSelectBoxesSuccess>(
+          DowntimeActionTypes.GetDowntimeDataSelectBoxesSuccess
+        )
+      ),
+    { dispatch: false }
+  );
 }
 
 /**
@@ -139,7 +145,7 @@ export function converApiDataToChartData(
 
   const lineName: string = data.length > 0 ? Object.keys(data[0])[2] : "";
   const barData: DowntimeModelsUI.ComboBarChartDataModelUI[] = [];
-  const lineSeries = [];
+  const lineSeries: any[] = [];
 
   data.forEach((elem: MimsModels.IDowntimeMachineParetoDto) => {
     barData.push({
@@ -190,20 +196,11 @@ export function convertApiDataToTableData(
 }
 
 export function convertApiDataToSelectBoxes(data: any[]): any[] {
-  const convertedData: any[] = [];
-  let element: MimsSelectBoxModel[] = [];
-
-  data.forEach((arrayForSelectBox) => {
-    arrayForSelectBox.forEach((elem) => {
-      element.push({
-        name: elem.Name,
-        value: elem.Id,
-        selected: false,
-      });
-    });
-    convertedData.push(element);
-    element = [];
-  });
-
-  return convertedData;
+  return data.map((arrayForSelectBox) =>
+    arrayForSelectBox.map((elem: any) => ({
+      name: elem.Name,
+      value: elem.Id,
+      selected: false,
+    }))
+  );
 }
