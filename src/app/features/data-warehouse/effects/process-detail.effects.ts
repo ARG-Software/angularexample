3b67d2193d23b19910389f574f2b32cb5ee3083a
blekso <mihael.istvan@gmail.com>
2025-02-20 12:25:36 +0100

    fixed process-detail.effects

--- src/app/features/data-warehouse/effects/process-detail.effects.ts
+++ src/app/features/data-warehouse/effects/process-detail.effects.ts
@@ -1,166 +1,183 @@
-import { Injectable } from '@angular/core';
-import { IMachineService } from '@api/services/interfaces/core/imachine.service';
-import { MimsSelectBoxModel } from '@mimsUI/input/select-box/models/select-box.model';
-import { IProcessDetailMachiningService } from '@api/services/interfaces/core/data-warehouse/iprocess-detail.service';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import { of } from 'rxjs/observable/of';
-import { catchError, finalize, map, switchMap, tap, min } from 'rxjs/operators';
-import * as loadingActions from '../../../main/actions/loading.actions';
-import * as fromMain from '../../../main/main.reducers.index';
+import { Injectable } from "@angular/core";
+import { IMachineService } from "@api/services/interfaces/core/imachine.service";
+import { MimsSelectBoxModel } from "@mimsUI/input/select-box/models/select-box.model";
+import { IProcessDetailMachiningService } from "@api/services/interfaces/core/data-warehouse/iprocess-detail.service";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Store } from "@ngrx/store";
+import { of } from "rxjs";
+import { catchError, finalize, map, switchMap, tap } from "rxjs/operators";
+import * as loadingActions from "../../../main/actions/loading.actions";
+import * as fromMain from "../../../main/main.reducers.index";
+
 import {
-    GetProcessDetailData,
-    GetProcessDetailDataSuccess,
-    ProcessDetailActionTypes,
-    ProcessDetailFailure,
-    GetProcessDetailDataSelectBoxes,
-    GetProcessDetailDataSelectBoxesSuccess
-} from '../actions/process-detail.actions';
+  GetProcessDetailData,
+  GetProcessDetailDataSuccess,
+  ProcessDetailActionTypes,
+  ProcessDetailFailure,
+  GetProcessDetailDataSelectBoxes,
+  GetProcessDetailDataSelectBoxesSuccess,
+} from "../actions/process-detail.actions";
+
 import {
-    ProcessDetailDataModelUI,
-    ProcessDetailTableModelUI,
-    ProcessDetailTableInformationModelUI,
-    ProcessDetailChartModelUI,
-    ProcessDetailValuesChartModelUI
-} from '../models/process-detail.models';
-import { Observable } from 'rxjs';
+  ProcessDetailDataModelUI,
+  ProcessDetailTableModelUI,
+  ProcessDetailTableInformationModelUI,
+  ProcessDetailChartModelUI,
+} from "../models/process-detail.models";
 
 @Injectable()
 export class ProcessDetailEffects {
+  constructor(
+    private processDetailService: IProcessDetailMachiningService,
+    private machineService: IMachineService,
+    private actions$: Actions,
+    private mainStore$: Store<fromMain.MainState>
+  ) {}
 
-    @Effect()
-    public getProcessDetailData$ = this.actions$.pipe(
-        ofType<GetProcessDetailData>(ProcessDetailActionTypes.GetProcessDetailData),
-        tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
-        map((action) => {
-            return action.payload;
-        }),
-        switchMap((payload) =>
+  public getProcessDetailData$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<GetProcessDetailData>(
+        ProcessDetailActionTypes.GetProcessDetailData
+      ),
+      tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
+      map((action) => action.payload),
+      switchMap((payload) =>
+        of([
+          {
+            Id: null,
+            MachineState: "Not Scheduled",
+            Reason: "",
+            Duration: 60,
+            StartTime: "2018-11-08T00:00:00",
+            EndTime: "2018-11-08T01:00:00",
+          },
+          {
+            Id: null,
+            MachineState: "In Production",
+            Reason: "",
+            Duration: 300,
+            StartTime: "2018-11-08T01:00:00",
+            EndTime: "2018-11-08T06:00:00",
+          },
+          {
+            Id: null,
+            MachineState: "Unplanned Downtime",
+            Reason: "Tool Change",
+            Duration: 20,
+            StartTime: "2018-11-08T06:00:00",
+            EndTime: "2018-11-08T06:20:00",
+          },
+          {
+            Id: null,
+            MachineState: "In Production",
+            Reason: "",
+            Duration: 360,
+            StartTime: "2018-11-08T06:20:00",
+            EndTime: "2018-11-08T12:20:00",
+          },
+          {
+            Id: null,
+            MachineState: "Unplanned Downtime",
+            Reason: "No reason given",
+            Duration: 100,
+            StartTime: "2018-11-08T12:20:00",
+            EndTime: "2018-11-08T14:00:00",
+          },
+          {
+            Id: null,
+            MachineState: "In Production",
+            Reason: "",
+            Duration: 20,
+            StartTime: "2018-11-08T14:00:00",
+            EndTime: "2018-11-08T14:20:00",
+          },
+          {
+            Id: null,
+            MachineState: "Not Scheduled",
+            Reason: "",
+            Duration: 360,
+            StartTime: "2018-11-08T14:20:00",
+            EndTime: "2018-11-08T20:00:00",
+          },
+        ])
+          // TODO: When backend is finished, uncomment service and the load tests
+          // this.processDetailService.GetProcessDetailData(payload)
+          .pipe(
+            map((response: any[]) => {
+              const processDetailData: ProcessDetailDataModelUI = {
+                Chart: convertApiDataToProcessDetailChartModelUI(response),
+                Table: convertApiDataToProcessDetailTableModelUI(response),
+              };
 
-            Observable.of(
-                [
-                    {
-                        Id: null,
-                        MachineState: 'Not Scheduled',
-                        Reason: '',
-                        Duration: 60,
-                        StartTime: '2018-11-08T00:00:00',
-                        EndTime: '2018-11-08T01:00:00'
-                    },
-                    {
-                        Id: null,
-                        MachineState: 'In Production',
-                        Reason: '',
-                        Duration: 300,
-                        StartTime: '2018-11-08T01:00:00',
-                        EndTime: '2018-11-08T06:00:00'
-                    },
-                    {
-                        Id: null,
-                        MachineState: 'Unplanned Downtime',
-                        Reason: 'Tool Change',
-                        Duration: 20,
-                        StartTime: '2018-11-08T06:00:00',
-                        EndTime: '2018-11-08T06:20:00'
-                    },
-                    {
-                        Id: null,
-                        MachineState: 'In Production',
-                        Reason: '',
-                        Duration: 360,
-                        StartTime: '2018-11-08T06:20:00',
-                        EndTime: '2018-11-08T12:20:00'
-                    },
-                    {
-                        Id: null,
-                        MachineState: 'Unplanned Downtime',
-                        Reason: 'No reason given',
-                        Duration: 100,
-                        StartTime: '2018-11-08T12:20:00',
-                        EndTime: '2018-11-08T14:00:00'
-                    },
-                    {
-                        Id: null,
-                        MachineState: 'In Production',
-                        Reason: '',
-                        Duration: 20,
-                        StartTime: '2018-11-08T14:00:00',
-                        EndTime: '2018-11-08T14:20:00'
-                    },
-                    {
-                        Id: null,
-                        MachineState: 'Not Scheduled',
-                        Reason: '',
-                        Duration: 360,
-                        StartTime: '2018-11-08T14:20:00',
-                        EndTime: '2018-11-08T20:00:00'
-                    }
-                ]
-            )
-            // TODO: When backend is finished, uncomment service and the load tests
-            // this.processDetailService.GetProcessDetailData(payload)
-            .pipe(
-                map((response: any[]) => {
+              return new GetProcessDetailDataSuccess(processDetailData);
+            }),
+            finalize(() =>
+              this.mainStore$.dispatch(new loadingActions.HideLoading())
+            ),
+            catchError((error) => {
+              return of(new ProcessDetailFailure(error));
+            })
+          )
+      )
+    )
+  );
 
-                    const oeeData: ProcessDetailDataModelUI = {
-                        Chart: convertApiDataToProcessDetailChartModelUI(response),
-                        Table: convertApiDataToProcessDetailTableModelUI(response)
-                    };
-
-                    return new GetProcessDetailDataSuccess(oeeData);
-                }),
-                finalize(() => this.mainStore$.dispatch(new loadingActions.HideLoading())),
-                catchError((error) => {
-                    return of(new ProcessDetailFailure(error));
-                })
-            )
+  public getProcessDetailDataSelectBox$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<GetProcessDetailDataSelectBoxes>(
+        ProcessDetailActionTypes.GetProcessDetailDataSelectBoxes
+      ),
+      tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
+      switchMap(() =>
+        this.machineService.GetMachines().pipe(
+          map((response: any[]) => {
+            return new GetProcessDetailDataSelectBoxesSuccess(
+              convertApiDataToSelectBox(response)
+            );
+          }),
+          finalize(() =>
+            this.mainStore$.dispatch(new loadingActions.HideLoading())
+          ),
+          catchError((error) => {
+            return of(new ProcessDetailFailure(error));
+          })
         )
-    );
+      )
+    )
+  );
 
-    @Effect({ dispatch: false })
-    public getProcessDetailDataSuccess$ = this.actions$.pipe(
-        ofType<GetProcessDetailDataSuccess>(ProcessDetailActionTypes.GetProcessDetailDataSuccess)
-    );
-
-    @Effect()
-    public getProcessDetailDataSelectBox$ = this.actions$.pipe(
-        ofType<GetProcessDetailDataSelectBoxes>(ProcessDetailActionTypes.GetProcessDetailDataSelectBoxes),
-        tap(() => this.mainStore$.dispatch(new loadingActions.ShowLoading())),
-        switchMap(() =>
-            this.machineService.GetMachines()
-            .pipe(
-                map((response: any[]) => {
-                    return new GetProcessDetailDataSelectBoxesSuccess(convertApiDataToSelectBox(response));
-                }),
-                finalize(() => this.mainStore$.dispatch(new loadingActions.HideLoading())),
-                catchError((error) => {
-                    return of(new ProcessDetailFailure(error));
-                })
-            )
+  public getProcessDetailDataSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<GetProcessDetailDataSuccess>(
+          ProcessDetailActionTypes.GetProcessDetailDataSuccess
         )
-    );
+      ),
+    { dispatch: false }
+  );
 
-    @Effect({ dispatch: false })
-    public getProcessDetailDataSelectBoxSuccess$ = this.actions$.pipe(
-        ofType<GetProcessDetailDataSelectBoxesSuccess>(ProcessDetailActionTypes.GetProcessDetailDataSelectBoxesSuccess)
-    );
+  public getProcessDetailDataSelectBoxSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<GetProcessDetailDataSelectBoxesSuccess>(
+          ProcessDetailActionTypes.GetProcessDetailDataSelectBoxesSuccess
+        )
+      ),
+    { dispatch: false }
+  );
 
-    @Effect({ dispatch: false })
-        public processDetailFailure$ = this.actions$.pipe(
-        ofType<ProcessDetailFailure>(ProcessDetailActionTypes.ProcessDetailFailure),
+  public processDetailFailure$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<ProcessDetailFailure>(
+          ProcessDetailActionTypes.ProcessDetailFailure
+        ),
         tap((error) => {
-            console.log('error');
+          console.error("Error:", error);
         })
-    );
-
-    public constructor(
-        private processDetailService: IProcessDetailMachiningService,
-        private machineService: IMachineService,
-        private actions$: Actions,
-        private mainStore$: Store<fromMain.MainState>
-    ) {}
+      ),
+    { dispatch: false }
+  );
 }
 
 /**
@@ -169,39 +186,29 @@ export class ProcessDetailEffects {
  * @returns data formated soo the data can be read by table
  */
 export function convertApiDataToProcessDetailTableModelUI(
-    data: any
+  data: any
 ): ProcessDetailTableInformationModelUI {
-    const tableData: ProcessDetailTableModelUI[] = [];
+  const tableData: ProcessDetailTableModelUI[] = data.map((elem: any) => ({
+    Id: null as number | null,
+    MachineState: elem.MachineState,
+    Reason: elem.Reason,
+    Duration: elem.Duration,
+    StartTime: elem.StartTime,
+    EndTime: elem.EndTime,
+  }));
 
-    data.forEach((elem) => {
-        tableData.push({
-            Id: null,
-            MachineState: elem.MachineState,
-            Reason: elem.Reason,
-            Duration: elem.Duration,
-            StartTime: elem.StartTime,
-            EndTime: elem.EndTime,
-        });
-    });
-
-    return {
-        Information: tableData,
-        Total: tableData.length
-    };
+  return {
+    Information: tableData,
+    Total: tableData.length,
+  };
 }
 
 export function convertApiDataToSelectBox(data: any[]): MimsSelectBoxModel[] {
-    const element: MimsSelectBoxModel[] = [];
-
-    data.forEach((elem) => {
-        element.push({
-            name: elem.Name,
-            value: elem.Id,
-            selected: false
-        });
-    });
-
-    return element;
+  return data.map((elem) => ({
+    name: elem.Name,
+    value: elem.Id,
+    selected: false,
+  }));
 }
 
 /**
@@ -209,24 +216,21 @@ export function convertApiDataToSelectBox(data: any[]): MimsSelectBoxModel[] {
  * @param data data from api to be manipulated
  * @returns data formated soo the data can be read by chart
  */
-export function convertApiDataToProcessDetailChartModelUI(data: any[]): ProcessDetailChartModelUI[] {
-
-    const series: ProcessDetailValuesChartModelUI[] = [];
-
-    data.forEach((elem) => {
-        series.push({
-            name: new Date(elem.EndTime).toISOString(),
-            value: elem.Duration,
-            extra: {
-                startTime : elem.StartTime,
-                endTime : elem.EndTime,
-                machineState: elem.MachineState
-            }
-        });
-    });
-
-    return [{
-        name: '',
-        series
-    }];
+export function convertApiDataToProcessDetailChartModelUI(
+  data: any[]
+): ProcessDetailChartModelUI[] {
+  return [
+    {
+      name: "",
+      series: data.map((elem) => ({
+        name: new Date(elem.EndTime).toISOString(),
+        value: elem.Duration,
+        extra: {
+          startTime: elem.StartTime,
+          endTime: elem.EndTime,
+          machineState: elem.MachineState,
+        },
+      })),
+    },
+  ];
 }
--- src/app/features/data-warehouse/models/process-detail.models.ts
+++ src/app/features/data-warehouse/models/process-detail.models.ts
@@ -1,62 +1,67 @@
-import * as faker from 'faker/locale/en_US';
-import * as Factory from 'factory.ts';
+import * as faker from "faker/locale/en_US";
+import * as Factory from "factory.ts";
 
 export interface ProcessDetailValuesChartModelUI {
-    name: string | Date;
-    value: any;
-    extra: any;
+  name: string | Date;
+  value: any;
+  extra: any;
 }
 
 export interface ProcessDetailChartModelUI {
-    name: string;
-    series: ProcessDetailValuesChartModelUI[];
+  name: string;
+  series: ProcessDetailValuesChartModelUI[];
 }
 
 export interface ProcessDetailTableModelUI {
-    Id?: number;
-    MachineState: string;
-    Reason: string;
-    Duration: number;
-    StartTime: Date;
-    EndTime: Date;
+  Id?: number | null;
+  MachineState: string;
+  Reason: string;
+  Duration: number;
+  StartTime: Date;
+  EndTime: Date;
 }
 
 export interface ProcessDetailTableInformationModelUI {
-    Information: ProcessDetailTableModelUI[];
-    Total: number;
+  Information: ProcessDetailTableModelUI[];
+  Total: number;
 }
 
 export interface ProcessDetailDataModelUI {
-    Chart: ProcessDetailChartModelUI[];
-    Table: ProcessDetailTableInformationModelUI;
+  Chart: ProcessDetailChartModelUI[];
+  Table: ProcessDetailTableInformationModelUI;
 }
 
-export const ProcessDetailValuesChartModelUIFactory = Factory.makeFactory<ProcessDetailValuesChartModelUI>({
+export const ProcessDetailValuesChartModelUIFactory =
+  Factory.makeFactory<ProcessDetailValuesChartModelUI>({
     name: faker.random.word(),
     value: faker.random.number(),
-    extra: null
-});
+    extra: null,
+  });
 
-export const ProcessDetailChartModelUIFactory = Factory.makeFactory<ProcessDetailChartModelUI>({
+export const ProcessDetailChartModelUIFactory =
+  Factory.makeFactory<ProcessDetailChartModelUI>({
     name: faker.random.word(),
-    series: ProcessDetailValuesChartModelUIFactory.buildList(3)
-});
+    series: ProcessDetailValuesChartModelUIFactory.buildList(3),
+  });
 
-export const ProcessDetailTableModelUIFactory = Factory.makeFactory<ProcessDetailTableModelUI>({
+export const ProcessDetailTableModelUIFactory =
+  Factory.makeFactory<ProcessDetailTableModelUI>({
     Id: faker.random.number(),
     MachineState: faker.random.word(),
     Reason: faker.random.word(),
     Duration: faker.random.number(),
     StartTime: faker.date.recent(),
-    EndTime: faker.date.recent()
-});
+    EndTime: faker.date.recent(),
+  });
 
-export const ProcessDetailTableInformationModelUIFactory = Factory.makeFactory<ProcessDetailTableInformationModelUI>({
+export const ProcessDetailTableInformationModelUIFactory =
+  Factory.makeFactory<ProcessDetailTableInformationModelUI>({
     Information: ProcessDetailTableModelUIFactory.buildList(2),
-    Total: faker.random.number()
-});
+    Total: faker.random.number(),
+  });
 
-export const ProcessDetailDataModelUIFactory = Factory.makeFactory<ProcessDetailDataModelUI>({
+export const ProcessDetailDataModelUIFactory =
+  Factory.makeFactory<ProcessDetailDataModelUI>({
     Chart: ProcessDetailChartModelUIFactory.buildList(2),
-    Table: ProcessDetailTableInformationModelUIFactory.build()
-});
+    Table: ProcessDetailTableInformationModelUIFactory.build(),
+  });
