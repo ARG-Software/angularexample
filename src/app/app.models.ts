a1f074fb7f7a3a506fb2f948ffaaff55583e73e6
blekso <mihael.istvan@gmail.com>
2025-02-20 11:23:02 +0100

    fixed app.modes.ts

--- src/app/app.models.ts
+++ src/app/app.models.ts
@@ -1,20 +1,20 @@
-import * as faker from 'faker/locale/en_US';
-import * as Factory from 'factory.ts';
+import { faker } from "@faker-js/faker";
+import * as Factory from "factory.ts";
 
 export interface PagingModelUI {
-    CurrentIndex: number;
-    HowManyPerPage: number;
-    PropertyToOrderBy?: string;
-    Ordered?: boolean;
-    IsDescending?: boolean;
-    Total?: number;
+  CurrentIndex: number;
+  HowManyPerPage: number;
+  PropertyToOrderBy?: string;
+  Ordered?: boolean;
+  IsDescending?: boolean;
+  Total?: number;
 }
 
 export const PagingModelUIFactory = Factory.makeFactory<PagingModelUI>({
-    CurrentIndex: faker.random.number(),
-    HowManyPerPage: faker.random.number(),
-    PropertyToOrderBy: faker.random.word(),
-    Ordered: faker.random.boolean(),
-    IsDescending: faker.random.boolean(),
-    Total: faker.random.number()
+  CurrentIndex: faker.number.int({ min: 0, max: 100 }),
+  HowManyPerPage: faker.number.int({ min: 1, max: 50 }),
+  PropertyToOrderBy: faker.lorem.word(),
+  Ordered: faker.datatype.boolean(),
+  IsDescending: faker.datatype.boolean(),
+  Total: faker.number.int({ min: 0, max: 1000 }),
 });
