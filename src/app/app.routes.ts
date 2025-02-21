47dfe0151da8d7cc1dacc2b02a8b98d79580c0e5
blekso <mihael.istvan@gmail.com>
2025-02-21 13:18:26 +0100

    minor fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [
-  /* {
-    path: '',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
-  }, */
+  {
+    path: "",
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
+  },
 ];
